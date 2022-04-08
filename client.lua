df32b10417670dccb397b8d0fdc44021bea0f129
Ryan <ryanmturnbull@gmail.com>
2022-04-08 20:31:16 +1000

    Created workaround for stack overflow issue.

--- client.lua
+++ client.lua
@@ -1,7 +1,6 @@
 -- Set default balance
-WalletBalance = 100
-BankBalance = 2000
-
+WalletBalance = 50 -- 32 BIT INTEGER!! STACK OVERFLOW!!
+BankBalance = 5000 -- 32 BIT INTEGER!! STACK OVERFLOW!!
 StatSetInt("MP0_WALLET_BALANCE", WalletBalance, true)
 StatSetInt("BANK_BALANCE", BankBalance, true)
 
@@ -14,9 +13,21 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Show current balance in the corner like in GTA:O
+-- Main 0ms Loop
 Citizen.CreateThread(function()
     while true do
+
+        -- Correct stack overflow error
+        if WalletBalance > 2147483647 then
+            WalletBalance = 2147483647
+            ShowNotification("Your wallet balance exceeds the maximum supported value. Contact staff for assistance.")
+        end
+        if BankBalance > 2147483647 then
+            BankBalance = 2147483647
+            ShowNotification("Your bank balance exceeds the maximum supported value. Contact staff for assistance.")
+        end
+
+        -- Show HUD
         if IsControlPressed(0, 48) then
             N_0x170f541e1cadd1de(true)
             SetMultiplayerWalletCash()
@@ -26,6 +37,13 @@ Citizen.CreateThread(function()
             RemoveMultiplayerWalletCash()
             RemoveMultiplayerBankCash()
         end
+
         Citizen.Wait(0)
     end
 end)
+
+function ShowNotification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
